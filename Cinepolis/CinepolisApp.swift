e07e765cea4ad7a8bc961b99dc4501579a3fd9f5
edsonmaya <edson.maya@iest.edu.mx>
2024-11-18 14:51:21 -0600

    Initial Commit

---
+++ Cinepolis/CinepolisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinepolisApp.swift
+//  Cinepolis
+//
+//  Created by Edson Maya Méndez on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CinepolisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinepolisTests/CinepolisTests.swift
@@ -0,0 +1,16 @@
+//
+//  CinepolisTests.swift
+//  CinepolisTests
+//
+//  Created by Edson Maya Méndez on 18/11/24.
+//
+
+import Testing
+
+struct CinepolisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
